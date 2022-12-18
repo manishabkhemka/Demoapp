aacae1a5cf04a49987654f8c04c3837794ccf7a3
manishabkhemka <manishabkhemka@gmail.com>
2022-12-18 10:20:41 -0500

    First commit with Git

---
+++ Demo/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		int[] a= {10,20,30,40};
+		for(int b:a)
+			System.out.println("Hello World: "+b);
+
+	}
+
+}
---
+++ Demo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author getma
+ *
+ */
+module Demo {
+}
